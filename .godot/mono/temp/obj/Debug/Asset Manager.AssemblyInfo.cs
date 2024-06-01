e03e3cf8b0104315443835d5fdb9d1189ec05ff5
Mehdi Saleh <94439809+Mehdi-Saleh@users.noreply.github.com>
2024-06-01 06:50:16 +0330

    Added some common formats to check for

--- .godot/mono/temp/obj/Debug/Asset Manager.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/Asset Manager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Asset Manager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8e291c8938d97efbba609c795532fa39603e16b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32ea0abcfaaf7f7347d2f5b7a121107f14399030")]
 [assembly: System.Reflection.AssemblyProductAttribute("Asset Manager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Asset Manager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
